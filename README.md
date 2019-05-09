8dd14263d56a6dc2e989810431c7d543aeaa8fab
lucafluri <luca.fluri4@gmail.com>
2019-05-09 15:55:48 +0200

    V2

--- README.md
+++ README.md
@@ -1,8 +1,6 @@
 # Bonus Assignment (Huffman Code) for mada@FHNW in SS19  
 
 
-
-
 This program is able to:
 
 - [x] Read in ASCII text file  
@@ -15,3 +13,4 @@ This program is able to:
 - [x] Store byte array in _output.dat_
 - [x] Decode  from external files and save decoded string in _decompress.txt_
 - [x] Decode _output-mada.dat_ with _dec-mada.txt_
+
--- src/Huffman_Main.java
+++ src/Huffman_Main.java
@@ -1,5 +1,3 @@
-import sun.reflect.generics.tree.Tree;
-
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -8,63 +6,56 @@ import java.util.*;
 
 public class Huffman_Main {
 
+    private static String inputPath = "ascii.txt";          // Ascii Text File to decode
+    private static String tablePath = "dec_tab.txt";        // Path to Huffman Table
+    private static String comprPath = "output.dat";         // Path to compressed File
+    private static String decomprPath = "decompress.txt";   // Path to decompressed File
+
     public static void main(String[] args) throws IOException {
-        String text = readASCIIFile("ascii.txt");
-        TreeMap<String, String> huffmanTable = createHuffmanCode(createFreqTable(text));
-        //System.out.println(huffmanTable);
-        encode(text, huffmanTable);
+        compress(); // creates freq table and creates a huffman tree which is used to compress/encode the original text
+                    // Both the Huffman table and the compressed text are stored.
 
-
-        decode();
+        decompress(); // Decompresses the specified file with the specified table and saves the result.
     }
 
     public static String readASCIIFile(String filepath) throws IOException {
-        String input = new String(Files.readAllBytes(Paths.get(filepath)));
-        //System.out.println(input);
-        System.out.println("Reading File...");
-
-        return input;
+        return new String(Files.readAllBytes(Paths.get(filepath)));
     }
 
     public static int[] createFreqTable(String input){
-        int[] freqTable = new int[256]; //ASCII Table Size = 256
+        System.out.println("Creating Frequency Table...");
+
+        int[] freqTable = new int[256]; //ASCII Table Size = 256, index is used as char identifier and elements as frequency.
         for(char c : input.toCharArray()){
             if(c<256){ //only ascii characters!
                 freqTable[c]++;
             }
         }
-
-        //for(int i : freqTable){System.out.print(i);}
-
         return freqTable;
     }
 
     public static TreeMap<String, String> createHuffmanCode(int[] freqTable) throws IOException{
+        System.out.println("Creating Huffman Tree...");
+
         PriorityQueue<Node> queue = new PriorityQueue<>((o1, o2) -> (o1.freq < o2.freq) ? -1 : 1); // sorted frequency table with (freq, char int)
-        TreeMap<String, String> map = new TreeMap<>(); //Huffmancode table
+        TreeMap<String, String> map = new TreeMap<>(); //Huffmancode table as treemap
         for(char i=0; i<freqTable.length; i++){
-            if((freqTable[i] > 0) && queue.add(new Node(freqTable[i], String.valueOf(i))));
+            if((freqTable[i] > 0) && queue.add(new Node(freqTable[i], String.valueOf(i)))); //saves every character and corresponding frequency that appears more than once in the text.
         } //frequency table generated
 
         Node left, right, top = null;
 
-        while(queue.size()!=1){
+        while(queue.size()!=1){ //Huffman Tree Algorithm
             left = queue.poll();
-            //System.out.println("Left: " + left);
-            right = queue.poll();
-            //System.out.println("Right: " + right);
-            top = new Node(left, right);
-            queue.add(top);
-
-            //System.out.println(queue);
+            right = queue.poll(); //removes the 2 nodes with lowest frequency
+            top = new Node(left, right); // creates a parent node with the combined frequency fo left and right nodes. -> see Node Class at bottom
+            queue.add(top); //Adds Parent Node to queue.
+            //Repeats until only one element is present in queue -> Algorithm finished.
         }
         //"Tree" finished, we have top node of tree
 
-        getCodes(map, "", top);
-        saveHuffman(map);
-
-
-        //System.out.println(map);
+        getCodes(map, "", top); //We use the top node to recursively traverse the Tree and generate the codes.
+        saveHuffman(map); //Save the generated Huffman Code
 
         return map;
 
@@ -72,35 +63,36 @@ public class Huffman_Main {
     }
 
     public static void getCodes(TreeMap<String, String> map, String code, Node top){
-        if(top.left == null && top.right==null){
-            map.put(top.value, code);
+        if(top.left == null && top.right==null){ //This means the node is a leaf node and we have found a character.
+            map.put(top.value, code); //Add the newly found character and huffman code pair to the map.
         }else{
-            getCodes(map, code + "1", top.left);
-            getCodes(map, code + "0", top.right);
+            getCodes(map, code + "0", top.left); //If we go left we add a 0 to the code and call the function recursively
+            getCodes(map, code + "1", top.right); //right -> +1
         }
 
     }
 
     public static void saveHuffman(TreeMap<String, String> table) throws IOException {
-        String path = "dec_tab.txt";
         String toWrite = "";
         TreeMap<String, String> map = (TreeMap) table.clone();
 
-
+        System.out.println("Saving Huffman Tree to " + tablePath + "...");
 
         int size = map.size();
-        for(int i = 0; i<size; i++){
+        for(int i = 0; i<size; i++){ //Saving Huffman Table according to Assignment
             toWrite += Integer.valueOf(map.firstEntry().getKey().toCharArray()[0]) + ":";
-            toWrite += map.firstEntry().getValue() + "-";
+            toWrite += map.firstEntry().getValue() + "-"; //The last "-" is ignored for simplicity
             map.pollFirstEntry();
         }
-
-
-        Files.write(Paths.get(path), toWrite.getBytes());
+        Files.write(Paths.get(tablePath), toWrite.getBytes()); //saves the Huffman Table to the specified file path
     }
 
-    public static void encode(String text, TreeMap<String, String> table) throws IOException{
+    public static void compress() throws IOException{
+        System.out.println("COMPRESSING:\nReading File " + inputPath + "...");
         String bitString = "";
+        String text = readASCIIFile(inputPath);
+        TreeMap<String, String> table = createHuffmanCode(createFreqTable(text)); //creates the huffman table
+        System.out.println("Compressing Text...");
 
         for(char c : text.toCharArray()){
             bitString += table.get(String.valueOf(c));
@@ -119,14 +111,17 @@ public class Huffman_Main {
         }
         //System.out.println(byteArray);
 
-        FileOutputStream fos = new FileOutputStream("output.dat");
+
+        System.out.println("Saving Compressed Text to " + comprPath + "...\n");
+        FileOutputStream fos = new FileOutputStream(comprPath);
         fos.write(byteArray);
         fos.close();
 
     }
 
-    public static void decode() throws FileNotFoundException, IOException{
-        File file = new File("output.dat");
+    public static void decompress() throws FileNotFoundException, IOException{
+        File file = new File(comprPath);
+        System.out.println("DECOMPRESSING:\nDecompressing " + comprPath + "...");
         byte[] bFile = new byte[(int) file.length()];
         FileInputStream fis = new FileInputStream(file);
         fis.read(bFile);
@@ -143,9 +138,10 @@ public class Huffman_Main {
         }
 
         bitString = bitString.substring(0, bitString.lastIndexOf("1"));
-        System.out.println(bitString);
+        //System.out.println(bitString);
 
-        String decTable = readASCIIFile("dec_tab.txt");
+        String decTable = readASCIIFile(tablePath);
+        System.out.println("Reading Huffman Table from " + tablePath + "...");
         //Parse String into TreeMap
         TreeMap<String, String> table = new TreeMap<>();
         String[] mappings = decTable.split("-");
@@ -156,7 +152,7 @@ public class Huffman_Main {
             table.put(c, v);
         }
 
-        System.out.println("read Table: " + table);
+        //System.out.println("read Table: " + table);
 
         String decoded = "";
         int a = 0;
@@ -172,11 +168,12 @@ public class Huffman_Main {
 
         }
 
-        System.out.println(decoded);
+        //System.out.println(decoded);
 
 
         //Write decoded File
-        Files.write(Paths.get("decompress.txt"), decoded.getBytes());
+        Files.write(Paths.get(decomprPath), decoded.getBytes());
+        System.out.println("Decompressed File saved to: " + decomprPath);
 
 
     }
@@ -186,7 +183,7 @@ public class Huffman_Main {
 
 }
 
-class Node implements Comparable<Node>{
+class Node{
     Node left = null;
     Node right = null;
     String value;
@@ -209,13 +206,6 @@ class Node implements Comparable<Node>{
         this.value = _left.value + _right.value;
         this.freq = _left.freq + _right.freq;
     }
-
-
-    @Override
-    public int compareTo(Node o) {
-        return this.freq - o.freq;
-    }
-
 
     @Override
     public String toString(){
